dec46664e8396043d1352d87c804720fca566382
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-30 10:41:37 +0200

    refactor(CartProduct, CartProductRepository): convert CartProduct to JPA entity

---
+++ src/main/kotlin/ecommerce/jpaEntity/CartProduct.kt
@@ -0,0 +1,31 @@
+package ecommerce.jpaEntity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+
+@Entity
+@Table(
+    name = "cart_products",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "product_id"])],
+)
+class CartProduct(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    var cart: Cart,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    var product: Product,
+    @Column(name = "quantity", nullable = false)
+    var quantity: Int = 1,
+)
