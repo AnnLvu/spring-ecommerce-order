6837d6c9924179bcd90b8154befdf9534b93c5a1
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-30 10:40:55 +0200

    refactor(Product, ProductRepository): convert Product to JPA ent

---
+++ src/main/kotlin/ecommerce/jpaEntity/Product.kt
@@ -0,0 +1,27 @@
+package ecommerce.jpaEntity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "products")
+class Product(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    @Column(name = "name", nullable = false, unique = true)
+    var name: String,
+    @Column(name = "price", nullable = false)
+    var price: Double,
+    @Column(name = "quantity", nullable = false)
+    var quantity: Int = 1,
+    @Column(name = "image_url", nullable = false, unique = true)
+    var imageUrl: String,
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+)
