11f09fcc7262c0a746ecb39de6bd19ae92a63516
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-30 12:14:33 +0200

    refactor(AdminProductService): handle JPA entity

--- src/main/kotlin/ecommerce/service/AdminProductService.kt
+++ src/main/kotlin/ecommerce/service/AdminProductService.kt
@@ -2,83 +2,105 @@ package ecommerce.service
 
 import ecommerce.dto.products.ProductDTO
 import ecommerce.dto.products.ProductPatchDTO
+import ecommerce.dto.products.ProductResponseDTO
 import ecommerce.exception.DuplicateProductNameException
 import ecommerce.exception.EntityNotFoundException
-import ecommerce.mapper.toDto
+import ecommerce.jpaEntity.Product
 import ecommerce.repository.ProductRepository
+import ecommerce.utils.toProductDTO
 import org.springframework.stereotype.Service
 import java.net.URI
 
 @Service
 class AdminProductService(private val productRepository: ProductRepository) {
-    fun getAllProducts(): List<ProductDTO> {
+    fun getAllProducts(): List<ProductResponseDTO> {
         val products = productRepository.findAll()
-        return products.map { it.toDto() }
+        return products.map { it.toProductDTO() }
     }
 
-    fun getProductById(id: Long): ProductDTO {
-        val product =
-            productRepository.findById(id)
-                ?: throw EntityNotFoundException("Product with id: $id not found")
-        return product.toDto()
+    fun getProductById(id: Long): ProductResponseDTO {
+        val product = getValidProduct(id)
+        return product.toProductDTO()
     }
 
-    fun createProduct(product: ProductDTO): URI {
-        if (productRepository.existsByName(product.name)) {
-            throw DuplicateProductNameException(product.name)
+    fun createProduct(productDTO: ProductDTO): URI {
+        if (productRepository.existsByName(productDTO.name)) {
+            throw DuplicateProductNameException(productDTO.name)
         }
-        val id = productRepository.create(product)
-        return URI.create("/products/$id")
+
+        val product =
+            productRepository.save(
+                Product(
+                    name = productDTO.name,
+                    price = productDTO.price,
+                    quantity = productDTO.quantity,
+                    imageUrl = productDTO.imageUrl,
+                ),
+            )
+        return URI.create("/products/${product.id}")
     }
 
     fun updateProduct(
         id: Long,
-        product: ProductDTO,
+        productDTO: ProductDTO,
     ) {
-        if (isDuplicateProductName(id, product.name)) {
-            throw DuplicateProductNameException(product.name)
-        }
-        if (productRepository.update(id, product) == 0) {
-            throw EntityNotFoundException("Product with id: $id not found")
+        val product =
+            getValidProduct(id)
+
+        product.name = productDTO.name
+        product.price = productDTO.price
+        product.quantity = productDTO.quantity
+        product.imageUrl = productDTO.imageUrl
+
+        try {
+            productRepository.save(product)
+        } catch (_: Exception) {
+            throw DuplicateProductNameException(productDTO.name)
         }
     }
 
     fun patchProduct(
         id: Long,
-        patch: ProductPatchDTO,
+        productPatchDTO: ProductPatchDTO,
     ) {
-        val existing =
-            productRepository.findById(id)
-                ?: throw EntityNotFoundException("Product with id: $id not found")
+        val existingProduct = getValidProduct(id)
 
-        if (patch.name != null && isDuplicateProductName(id, patch.name)) {
-            throw DuplicateProductNameException(patch.name)
+        productPatchDTO.name?.let { newName ->
+            if (newName != existingProduct.name && isDuplicateProductName(id, newName)) {
+                throw DuplicateProductNameException(newName)
+            }
+            existingProduct.name = newName
         }
 
-        val updatedProduct =
-            existing.copy(
-                name = patch.name ?: existing.name,
-                description = patch.description ?: existing.description,
-                price = patch.price ?: existing.price,
-                imageUrl = patch.imageUrl ?: existing.imageUrl,
-                quantity = patch.quantity ?: existing.quantity,
-            )
-
-        productRepository.update(id, updatedProduct.toDto())
-    }
-
-    fun deleteProduct(id: Long) {
-        if (productRepository.deleteById(id) == 0) {
-            productRepository.deleteById(id)
-            throw EntityNotFoundException("Product with id: $id not found")
+        productPatchDTO.price?.let {
+            existingProduct.price = it
         }
+
+        productPatchDTO.imageUrl?.let {
+            existingProduct.imageUrl = it
+        }
+
+        productPatchDTO.quantity?.let {
+            existingProduct.quantity = it
+        }
+
+        productRepository.save(existingProduct)
     }
 
     private fun isDuplicateProductName(
         id: Long,
         name: String,
     ): Boolean {
-        val oldProduct = productRepository.findByName(name)
+        val oldProduct = productRepository.findByName(name).orElse(null)
         return oldProduct != null && oldProduct.id != id
+    }
+
+    private fun getValidProduct(productId: Long): Product {
+        return productRepository.findById(productId).orElseThrow { EntityNotFoundException("Product with id $productId not found") }
+    }
+
+    fun deleteProduct(id: Long) {
+        val product = getValidProduct(id)
+        productRepository.delete(product)
     }
 }
