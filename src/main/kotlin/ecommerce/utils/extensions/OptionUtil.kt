7e53a4cf2eba69b5e5453eab4d2f90ed54b27ae6
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-01 23:42:18 +0200

    refactor(OptionDTO.toEntity): fix

--- src/main/kotlin/ecommerce/utils/extensions/OptionUtil.kt
+++ src/main/kotlin/ecommerce/utils/extensions/OptionUtil.kt
@@ -3,22 +3,22 @@ package ecommerce.utils.extensions
 import ecommerce.dto.products.OptionDTO
 import ecommerce.dto.products.OptionResponseDTO
 import ecommerce.model.Option
-import ecommerce.model.Product
 
-fun OptionDTO.toEntity(product: Product): Option {
+fun OptionDTO.toEntity(): Option {
     return Option(
         name,
         price,
         quantity,
-        product,
+        imageUrl,
     )
 }
 
 fun Option.toDTO(): OptionResponseDTO {
     return OptionResponseDTO(
-        this.id,
-        this.name,
-        this.price,
-        this.quantity,
+        id,
+        name,
+        price,
+        quantity,
+        imageUrl,
     )
 }
